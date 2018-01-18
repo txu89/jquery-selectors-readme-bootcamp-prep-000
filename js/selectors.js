9c389e534f838527d336387999726564a629028b
Tom Xu <tomxu89@gmail.com>
2018-01-18 04:20:22 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,5 +7,13 @@ function lastImageSelector() {
 }
 
 function ninjaBabySelector() {
-  return $('#')
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('#pic-list ul il:first-child')
 }
